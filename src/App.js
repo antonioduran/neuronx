a646382df1a04264904e0bff742bc3cd79633544
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2023-09-16 16:32:51 -0600

    [ Redux - Sagas - User ]

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from './store/user/user.action';
+//import { onAuthStateChangedListener, createUserDocumentFromAuth, getCurrentUser } from "./utils/firebase/firebase.utils";
+import { checkUserSession } from './store/user/user.action';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(()=> {
-    const unsubscribe =  onAuthStateChangedListener(async (user)=> {
-        if (user) {
-            await createUserDocumentFromAuth(user);
-        }
-        console.log("-----user::: " + JSON.stringify(user))
-        dispatch(setCurrentUser(user));
-    });
+    // const unsubscribe =  onAuthStateChangedListener(async (user)=> {
+    //     if (user) {
+    //         await createUserDocumentFromAuth(user);
+    //     }
+    //     console.log("-----user::: " + JSON.stringify(user))
+    //     dispatch(setCurrentUser(user));
+    // });
 
-    return unsubscribe;
+    // return unsubscribe;
+    //getCurrentUser().then((user) => console.log('::::::::::::: ' + JSON.stringify(user)))
+    dispatch(checkUserSession());
 }, [dispatch])
   return (
     <Routes>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES }  from '../button/button.component';
-import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+//import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 import './sign-in-form.styles.scss';
+
+import {
+    googleSignInStart,
+    emailSignInStart,
+  } from '../../store/user/user.action';
 
 const defaultFormFields = {
     email : '',
@@ -10,6 +16,7 @@ const defaultFormFields = {
 }
 
 const SignInForm = ()=> {
+    const dispatch = useDispatch();
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {email, password} = formFields;
 
@@ -18,7 +25,8 @@ const SignInForm = ()=> {
     }
 
     const signInWithGoogle = async () => {
-        await signInWithGooglePopup();
+        dispatch(googleSignInStart());
+        //await signInWithGooglePopup();
     };
 
     const handleSubmit = async (event) => {
@@ -26,7 +34,8 @@ const SignInForm = ()=> {
         
         try {
             //const user = 
-            await signInAuthUserWithEmailAndPassword(email, password)
+            //await signInAuthUserWithEmailAndPassword(email, password)
+            dispatch(emailSignInStart(email, password));
             resetFormFields()
         } catch (error) {
             switch (error.code) {
@@ -39,6 +48,8 @@ const SignInForm = ()=> {
                 default:
                     console.log(error);
             }
+
+            console.log('user sign in failed', error);
         }
     }
 
